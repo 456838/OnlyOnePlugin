b4e9585696e0097ed0448fdae3ddf98d79b4376c
salton <563185579@qq.com>
2018-05-28 11:09:06 +0800

    update xmly

---
+++ pluginxmly/xmly/src/main/java/com/salton123/xmly/fm/MiniPlayerComponent.kt
@@ -0,0 +1,18 @@
+package com.salton123.xmly.fm
+
+import android.os.Bundle
+import com.salton123.base.BaseSupportFragment
+import com.salton123.xmly.R
+
+class MiniPlayerComponent :BaseSupportFragment(){
+    override fun getLayout(): Int {
+        return R.layout.xmly_cp_mini_player
+    }
+
+    override fun initVariable(savedInstanceState: Bundle?) {
+    }
+
+    override fun initViewAndData() {
+    }
+
+}
--- pluginxmly/xmly/src/main/java/com/salton123/xmly/fm/XmlyComponent.kt
+++ pluginxmly/xmly/src/main/java/com/salton123/xmly/fm/XmlyComponent.kt
@@ -1,9 +1,13 @@
 package com.salton123.xmly.fm
 
 import android.os.Bundle
+import android.text.Html
+import android.view.View
 import com.salton123.base.BaseSupportFragment
 import com.salton123.base.FragmentDelegate
 import com.salton123.xmly.R
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import kotlinx.android.synthetic.main.xmly_cp_main.*
 
 
 /**
@@ -13,10 +17,16 @@ import com.salton123.xmly.R
  * Description:
  */
 class XmlyComponent : BaseSupportFragment() {
+
+    private val mRecommendComponent by lazy { FragmentDelegate.newInstance(RecommendComponent::class.java) }
+    private val mMiniPlayerComponent by lazy { FragmentDelegate.newInstance(MiniPlayerComponent::class.java) }
+    private val mMusicPlayerComponent by lazy { FragmentDelegate.newInstance(MusicPlayerComponent::class.java) }
+
     override fun initViewAndData() {
         if (findFragment(RecommendComponent::class.java) == null) {
-            loadRootFragment(R.id.fl_main_content, FragmentDelegate.newInstance(RecommendComponent::class.java))
-
+            loadRootFragment(R.id.fl_main_content, mRecommendComponent)
+            loadRootFragment(R.id.miniPlayerContainer, mMiniPlayerComponent)
+            loadRootFragment(R.id.musicPlayerContainer, mMusicPlayerComponent)
         }
     }
 
@@ -28,5 +38,51 @@ class XmlyComponent : BaseSupportFragment() {
 
     }
 
+    override fun initListener() {
+        slideUpdatePannel.addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
+            override fun onPanelSlide(panel: View?, slideOffset: Float) {
+                miniPlayerContainer.alpha = 1 - slideOffset
+            }
 
+            override fun onPanelStateChanged(panel: View?, previousState: SlidingUpPanelLayout.PanelState?, newState: SlidingUpPanelLayout.PanelState?) {
+            }
+        })
+        miniPlayerContainer.setOnClickListener { switchPanel() }
+    }
+
+    private fun switchPanel() {
+        if (slideUpdatePannel.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            collapsePanel()
+        } else {
+            expandPanel()
+        }
+    }
+
+    private fun collapsePanel() {
+        slideUpdatePannel.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
+    }
+
+    private fun expandPanel() {
+        slideUpdatePannel.panelState = SlidingUpPanelLayout.PanelState.EXPANDED
+    }
+
+    /**
+     * 处理回退事件
+     *
+     * @return
+     */
+    override fun onBackPressedSupport(): Boolean {
+        return if (slideUpdatePannel.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            collapsePanel()
+            true
+        } else {
+            val builder = android.support.v7.app.AlertDialog.Builder(_mActivity)
+            builder.setTitle("Hi").setMessage(Html.fromHtml("要退出App吗？")).setPositiveButton("嗯") { dialog, which ->
+                _mActivity.finish()
+                System.exit(0)
+            }.setNegativeButton("再看看", null)
+                    .show()
+            true
+        }
+    }
 }
